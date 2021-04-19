50a8caf85489a259c86acd4aed0f44fcab5e2dd7
Olha Pavliuk <olia.pavliuk@gmail.com>
2021-04-19 16:04:18 +0300

    add run button

--- VCTransitionAnimationsOverlap/FirstVC.swift
+++ VCTransitionAnimationsOverlap/FirstVC.swift
@@ -12,8 +12,20 @@ class FirstVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        addRunBtn()
     }
 
+    private func addRunBtn() {
+        let w: CGFloat = 100
+        let runBtn = UIButton(frame: CGRect(x: view.frame.width-w, y: 0, width: w, height: 40))
+        runBtn.backgroundColor = .blue
+        runBtn.setTitle("Run", for: .normal)
+        runBtn.addTarget(self, action: #selector(runBtnPressed), for: .touchUpInside)
+        view.addSubview(runBtn)
+    }
 
+    @objc func runBtnPressed() {
+        fatalError("TODO")
+    }
 }
 

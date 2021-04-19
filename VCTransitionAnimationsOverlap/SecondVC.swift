60e72f817c04aadeffba645f9b9117be9b608678
Olha Pavliuk <olia.pavliuk@gmail.com>
2021-04-19 15:58:00 +0300

    add First/Second VCs

--- VCTransitionAnimationsOverlap/SecondVC.swift
+++ VCTransitionAnimationsOverlap/SecondVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondVC.swift
 //  VCTransitionAnimationsOverlap
 //
 //  Created by Olha Pavliuk on 19.04.2021.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .green
     }
 
 
